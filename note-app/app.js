08d27067f81ff87d84fa45ab196854fbd416aadb
Naga Sivaji Sirigineedi <nagasivaji@Nagas-MacBook-Air.local>
2023-04-30 15:38:33 +0530

    added builder opting for title and body of the note

--- note-app/app.js
+++ note-app/app.js
@@ -1,15 +1,28 @@
 const yargs = require('yargs');
 
-// const cmd = yargs.argv
-// console.log(cmd)
-
 yargs.command({
     command: 'add',
     describe: 'add a new note',
-    handler: () => {
-        console.log('Adding new note...')
+    builder: {
+        title: {
+            describe: 'note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: (argv) => {
+        console.log('Adding note...')
+        console.log('title: ' + argv.title)
+        console.log('body: ' + argv.body)
     }
 })
+
+// cmd: ode app.js add --title="meeting@10AM" --body="Dev weekly syncup call"
 
 yargs.command({
     command: 'remove',
