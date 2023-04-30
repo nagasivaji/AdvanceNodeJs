df92bf15965ae5405a2a834ccd2c59302ff00c1c
Naga Sivaji Sirigineedi <nagasivaji@Nagas-MacBook-Air.local>
2023-04-30 11:37:15 +0530

    Validating Email using npm module called Validator

--- note-app/app.js
+++ note-app/app.js
@@ -1,6 +1,11 @@
-const utils = require('./utils')
+const validator = require('validator')
+    // const utils = require('./utils')
 
-console.log(utils.myName)
-console.log(utils.getFullName())
+// console.log(utils.myName)
+// console.log(utils.getFullName())
 
 // To init npm in our project use npm intt in root project directory
+
+// Validating Email using validator npm module
+console.log(validator.isEmail('sivaji'))
+console.log(validator.isEmail('sivaji@gmail.com'))
