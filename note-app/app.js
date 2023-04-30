f7d15904f5937ab909b8cb8b6d1bcbe8a8c2d24b
Naga Sivaji Sirigineedi <nagasivaji@Nagas-MacBook-Air.local>
2023-04-30 10:27:21 +0530

    append new lines into files

--- note-app/app.js
+++ note-app/app.js
@@ -1,2 +1,3 @@
 const fs = require('fs');
-fs.writeFileSync('note.txt', 'Hello... This file is created BY node JS')
+fs.writeFileSync('note.txt', 'Hello... This file is created BY node JS \n')
+fs.appendFileSync('note.txt', 'Hello NagaSivaji...')
