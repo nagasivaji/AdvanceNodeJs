3d204b8558d3f098e9b2662cb8213da462d09d9a
Naga Sivaji Sirigineedi <nagasivaji@Nagas-MacBook-Air.local>
2023-05-01 16:43:33 +0530

    Implemented fetch api for getting weather data

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,22 +1,17 @@
-console.log("Starting")
+// request url 
+// http://api.weatherstack.com/current?access_key=ed3ea9627bb950b22b35cd2b31f98c44&query=India
 
-setTimeout(() => {
-    console.log('Timeout')
-}, 2000)
-
-
-console.log("Stopping")
-
-
-/*
-
-1. First it will add console statement to stack and start executing it so it i will print Starting
-2. Second it will push this asynchronous (Timeout) code to Stack. Since it is an asynchronous code it will be moved to event stack and start timer
-3. Since node stack is empty the second console statement will added to the stack. Till timer is not completed by that time it will execute second console and print the statement as Stopping.
-4. Node staqck is empty now it will be in rest mode.
-5. Then if the timer is completed then the asynchronous code is moved to event queue.
-6. Since node will continuously check its stack and event queue. Now event queue is not empty it i will execute asynchronous code.
+fetch('http://api.weatherstack.com/current?access_key=ed3ea9627bb950b22b35cd2b31f98c44&query=India')
+    .then(res => res.json())
+    .then(json => {
+        // console.log(json)
+        const temperatureDetails = 'The current temperature in ' + json.location.name + ' is ' + json.current.temperature + ' degrees'
+        console.log(temperatureDetails)
+    })
 
 
-
-*/
+// fetch('https://jsonplaceholder.typicode.com/todos/?id=1')
+//     .then((res) => {
+//         return res.json()
+//     })
+//     .then(json => console.log(json))
