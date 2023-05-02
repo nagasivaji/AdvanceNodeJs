91c5b486ec5a343af055d204757bd5cac0414de6
Naga Sivaji Sirigineedi <nagasivaji@Nagas-MacBook-Air.local>
2023-05-02 22:18:41 +0530

    Rendering data from a static html page

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,8 @@
+const path = require('path');
 const express = require('express')
 
 const app = express()
+app.use(express.static(path.join(__dirname, '../public')))
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello World! </h1>')
