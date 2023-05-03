1c106657b0932e8d3de026b95dd45a76381f9e23
Naga Sivaji Sirigineedi <nagasivaji@Nagas-MacBook-Air.local>
2023-05-03 22:31:33 +0530

    Rendering pages using HBS(handelbars)

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,12 +2,15 @@ const path = require('path');
 const express = require('express')
 
 const app = express()
+
+app.set('view engine', 'hbs')
+
 app.use(express.static(path.join(__dirname, '../public')))
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World! </h1>')
-})
 
+app.get('/', (req, res) => {
+    res.render('index')
+})
 
 app.get('/about', (req, res) => {
     res.send({
