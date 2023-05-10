8f38a9f7f07f97328df8eec95367d45cdbdb2bfd
Naga Sivaji Sirigineedi <nagasivaji@Nagas-MacBook-Air.local>
2023-05-10 13:53:36 +0530

    Connecting to MongoDb from Node.js

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,1 +1,23 @@
 // Crud Operations
+// importing Mongodb
+const mongodb = require('mongodb')
+
+// Creating mongodb client
+const MongoClient = mongodb.MongoClient
+
+// Mongo Db Connection URL
+const connectionUrl = 'mongodb://127.0.0.1:27017'
+
+// Database Name
+const databaseName = 'task-manager'
+
+// Creating connection to MongoDB
+MongoClient.connect(connectionUrl, { useNewUrlParser: true }, (err, client) => {
+
+    if (err) {
+        return console.log('Unable to connect to MongoDB')
+    }
+
+    console.log('Connected to MongoDB')
+
+})
