b71294a94afb04d7fed016a2dc365d10349b75a0
Naga Sivaji Sirigineedi <nagasivaji@Nagas-MacBook-Air.local>
2023-05-10 21:21:28 +0530

    Making models to be more perfect with validations etc

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -16,19 +16,34 @@ mongoose.connect(connectionUrl)
 const User = mongoose.model('User', {
     name: {
         type: String,
-        required: true // Throws error is we do not specify name
+        required: true, // Throws error is we do not specify name
+        trim: true,
     },
     email: {
         type: String,
         required: true,
+        trim: true,
+        lowercase: true,
         validate(value) {
             if (!validator.isEmail(value)) {
                 throw new Error('Email is not valid')
             }
         }
     },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 7,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Password cannot contain "password"')
+            }
+        }
+    },
     age: {
         type: Number,
+        default: 0,
         validate(value) {
             if (value < 0) {
                 throw new Error('Age cannot be negative')
@@ -59,7 +74,8 @@ me.save()
 const Task = mongoose.model('Task', {
     description: {
         type: String,
-        required: true
+        required: true,
+        trim: true
     },
     completed: {
         type: Boolean,
